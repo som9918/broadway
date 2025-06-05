5ff71189835d154a30e2589384fe6c5faf3e3642
som9918 <37866799+som9918@users.noreply.github.com>
2025-06-05 21:56:36 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+// header bg change 
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 80) {
+            $(".black").css("background", "black");
+        }
+
+        else {
+            $(".black").css("background", "transparent");
+        }
+    })
+})
 
