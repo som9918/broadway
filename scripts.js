889bdce2ff9aaf39a4f361e3a08ec89d2d375262
som9918 <37866799+som9918@users.noreply.github.com>
2025-06-05 21:57:56 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 80) {
-            $(".black").css("background", "black");
+            $("#headerContainer").css("background", "black");
         }
 
         else {
-            $(".black").css("background", "transparent");
+            $("#headerContainer").css("background", "transparent");
         }
     })
 })
